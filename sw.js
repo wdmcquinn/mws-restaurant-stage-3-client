a162fc0071e4a374e771fb8658543847f84c4ff3
Doug McQuinn <wdmcquinn@gmail.com>
2018-06-06 11:29:42 -0400

    feature: Service Worker
    All local files are caching now as well as remote requests.
    I split the caches into a file cache and and a fetch cache so that if i update my
    files i can increment and it will update without affecting the remote fetched cache.

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const files = 'v2';
+const FILES = 'v1';
+const FETCHED = 'fetched';
 const cacheFiles = [ // List of files that need to cache on install
   './',
   './index.html',
@@ -23,10 +24,11 @@ const cacheFiles = [ // List of files that need to cache on install
 /**
  * Install the service worker and open the local cache.
  */
-self.addEventListener('install', (e) =>{
+
+self.addEventListener('install', (event) =>{
   console.log("[SERVICE_WORKER] Installed");
-  e.waitUntil(
-    caches.open(files)
+  event.waitUntil(
+    caches.open(FILES)
       .then(cache => cache.addAll(cacheFiles))
   );
 });
@@ -35,19 +37,43 @@ self.addEventListener('install', (e) =>{
  * Activate and clean up old caches.
  */
 
-self.addEventListener('activate', (e) =>{
+self.addEventListener('activate', (event) =>{
   console.log("[SERVICE_WORKER] Activated");
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (!files.includes(key)){
-          return caches.delete(key);
-        }
-      })
-    ))
-    .then(() => self.clients.claim())
+  const myCaches = [FILES, FETCHED];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !myCaches.includes(cacheName));
+    })
+    .then(oldCaches => {
+      return Promise.all(oldCaches.map(oldCache => {
+        return caches.delete(oldCache);
+      }));
+    })
   );
 });
 
+/**
+ * Check all request to see if they exist is cache. If so return them. If not
+ * clone the request and try to fetch it from the network. Check to make sure
+ * the response is valid and if so clone it and add it to the cache before
+ * returning it.
+ */
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      if (res) return res; // 
+      const reqCopy = event.request.clone(); 
+      return fetch(reqCopy).then(res => { 
+        if (!res || res.status != 200 || res.type !== 'basic') return res; 
+        const resCopy = res.clone();
+        caches.open(FETCHED).then(cache => {
+          cache.put(event.request, resCopy);
+        });
+        return res;
+      });
+    })
+  );
+});
 
 
